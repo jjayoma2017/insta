b59b6c216e88d7ed620b676b40bcb9f7d10f8667
jjayoma2017 <jjayoma2017@gmail.com>
2022-07-20 01:54:29 +0800

    implement a sticky header

--- components/Header.js
+++ components/Header.js
@@ -5,49 +5,51 @@ import { HomeIcon } from '@heroicons/react/solid';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      {/* hidden to be hidden at mobile */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
-          layout="fill"
-          className="object-contain"
-          alt=""
-        />
-      </div>
-
-      {/* hidden to be hidden at mobile */}
-      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-        <Image
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
-          layout="fill"
-          className="object-contain"
-          alt=""
-        />
-      </div>
-      {/* Middle */}
-      <div className="relative mt-1">
-        <div className="absolute top-2 left-2">
-          <SearchIcon className="h-5 text-gray-500" />{' '}
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        {/* hidden to be hidden at mobile */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+            layout="fill"
+            className="object-contain"
+            alt=""
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
-      {/* Right */}
-      <div className="flex space-x-4 items-center">
-        <HomeIcon className="hidden lg:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
-        <Image
-          src="https://media-exp2.licdn.com/dms/image/C5603AQH3J_1kphIvhQ/profile-displayphoto-shrink_400_400/0/1517429465337?e=1663804800&v=beta&t=RYZazLKN-2Ttsqcnn6riV0kqzVJfChjfn68_tIfNgio"
-          className="rounded-full cursor-pointer"
-          alt=""
-          width={50}
-          height={50}
-        />
+
+        {/* hidden to be hidden at mobile */}
+        <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
+            layout="fill"
+            className="object-contain"
+            alt=""
+          />
+        </div>
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <SearchIcon className="h-5 text-gray-500" />{' '}
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden lg:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <Image
+            src="https://media-exp2.licdn.com/dms/image/C5603AQH3J_1kphIvhQ/profile-displayphoto-shrink_400_400/0/1517429465337?e=1663804800&v=beta&t=RYZazLKN-2Ttsqcnn6riV0kqzVJfChjfn68_tIfNgio"
+            className="rounded-full cursor-pointer"
+            alt=""
+            width={50}
+            height={50}
+          />
+        </div>
       </div>
     </div>
   );
